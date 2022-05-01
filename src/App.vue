3ace067f1cd67454573f6571e72a383c8978c084
Carla-Jane-Mabutol <crolmabutol@gmail.com>
2022-05-02 03:58:17 +0800

    mymusic

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="app-container p-3 bg-black m-auto">
+    <SongList />
+  </div>
+</template>
+
+<script>
+import SongList from './components/SongList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    SongList
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: sans-serif;
+}
+.app-container {
+  width: 100%;
+  height: 50%;
+  padding-top: -30px;
+  padding-left: 50px;
+  padding-right: 50px;
+  padding-bottom: 50px;
+  border-color: gold;
+  border-width: 2px;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
